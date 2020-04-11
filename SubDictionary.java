30cb4629cb60c3ee0d223a09b877fa316bdf24c2
eli-samuel <0.0elisamuel@gmail.com>
2020-04-11 02:14:25 -0400

    delete a newline

--- SubDictionary.java
+++ SubDictionary.java
@@ -8,7 +8,7 @@ import java.io.FileNotFoundException;
 public class SubDictionary {
 
     public static void main(String[] args) {;
-        System.out.print("Enter file name: \n");
+        System.out.print("Enter file name: ");
 
         ArrayList<String> dict = new ArrayList<String>();
 
